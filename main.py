59bd616f5f4ce350c56a994f787c01e2bfaa75dc
Winston Muijs <winstonhmuijs@gmail.com>
2023-01-19 12:14:06 +0100

    Added Rectangles to the game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 
+
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 400
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -13,8 +14,11 @@ text_font = pygame.font.Font("./font/Pixeltype.ttf", 50)
 text = text_font.render("My firstGame", False, "dodgerblue4")
 
 enemy = pygame.image.load("./characters/1.png").convert_alpha()
+enemy_rect = enemy.get_rect(midbottom=(600, 300))
 enemy_x_pos = 600
 
+player = pygame.image.load("./characters/plater2.png").convert_alpha()
+player_rect = player.get_rect(midbottom=(80, 300))
 clock = pygame.time.Clock()
 
 while True:
@@ -25,9 +29,10 @@ while True:
     screen.blit(background_sky, (0, 0))
     screen.blit(background_ground, (0, 300))
     screen.blit(text, (300, 50))
-    enemy_x_pos -= 4
-    if enemy_x_pos < -100:
-        enemy_x_pos = 800
-    screen.blit(enemy, (enemy_x_pos, 250))
+    enemy_rect.right -= 4
+    if enemy_rect.right <= 0:
+        enemy_rect.left = 800
+    screen.blit(enemy, enemy_rect)
+    screen.blit(player, player_rect)
     pygame.display.update()
     clock.tick(60)
