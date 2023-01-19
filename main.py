c7915fd473529683ea159f325e78b7ba98c5ad5f
Winston Muijs <winstonhmuijs@gmail.com>
2023-01-19 16:54:37 +0100

    Added Keyboard input in event loop.

--- main.py
+++ main.py
@@ -27,9 +27,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEMOTION:
-            if player_rect.collidepoint(event.pos):
-                print("collision")
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print("collision")
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print('jump')
+        if event.type == pygame.KEYUP:
+            print('release button')
 
     screen.blit(background_sky, (0, 0))
     screen.blit(background_ground, (0, 300))
