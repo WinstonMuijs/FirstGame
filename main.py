6fd3b051ea33951be8723f9da276bae4dc685611
Winston Muijs <winstonhmuijs@gmail.com>
2023-01-20 14:55:16 +0100

    Added a score function in the form of time.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
 import sys
 
+
+def display_score():
+    current_time = pygame.time.get_ticks()
+    score = text_font.render(f"{current_time}", False, (97, 24, 237))
+    score_rect = score.get_rect(center=(WINDOW_WIDTH / 2, 60))
+    screen.blit(score, score_rect)
+
+
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 800, 400
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -10,8 +18,8 @@ background_sky = pygame.image.load("./characters/background.png").convert_alpha(
 background_ground = pygame.image.load("./characters/ground.png").convert_alpha()
 
 text_font = pygame.font.Font("./font/Pixeltype.ttf", 50)
-text = text_font.render("My firstGame", False, (97, 24, 237))
-text_rect = text.get_rect(center=(WINDOW_WIDTH / 2, 60))
+# text = text_font.render("My firstGame", False, (97, 24, 237))
+# text_rect = text.get_rect(center=(WINDOW_WIDTH / 2, 60))
 
 enemy = pygame.image.load("./characters/1.png").convert_alpha()
 enemy_rect = enemy.get_rect(midbottom=(600, 300))
@@ -43,11 +51,12 @@ while True:
                 if event.key == pygame.K_SPACE:
                     game_active = True
                     enemy_rect.x = 800
-    if game_active: # game
+    if game_active:  # game
         screen.blit(background_sky, (0, 0))
         screen.blit(background_ground, (0, 300))
-        pygame.draw.rect(screen, (226, 24, 135), text_rect.inflate(25, 25), width=8, border_radius=5)
-        screen.blit(text, text_rect)
+        # pygame.draw.rect(screen, (226, 24, 135), score_rect.inflate(25, 25), width=8, border_radius=5)
+        # screen.blit(text, score_rect)
+        display_score()
 
         enemy_rect.right -= 4
         if enemy_rect.right <= 0:
@@ -64,7 +73,7 @@ while True:
         # collision
         if player_rect.colliderect(enemy_rect):
             game_active = False
-    else: # intro
+    else:  # intro
         screen.fill('yellow')
 
     pygame.display.update()
